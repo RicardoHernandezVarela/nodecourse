2cc1f487b3460dc47b1b37d3998c1679fe7ce38f
RicardoHernandezVarela <rhvarela011192@gmail.com>
2021-08-02 16:07:42 -0500

    Use EJS template engine

--- expressProject/app.js
+++ expressProject/app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
+const mongo = 'mongodb+srv://rickdev:abril1997@cluster0.us2mq.mongodb.net/item-database?retryWrites=true&w=majority';
 
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    const items = [
+        {name: 'shoes', price: 250},
+        {name: 't-shirt', price: 150},
+        {name: 'jeans', price: 350}
+    ];
+
+    res.render('index', {items });
 });
 
 app.get('/contact', (req, res) => {
-    res.sendFile('./views/contact.html', {root: __dirname});
+    res.render('contact');
 });
 
 app.get('/add-item', (req, res) => {
-    res.sendFile('./views/add-item.html', {root: __dirname});
+    res.render('add-item');
 });
 
 app.use((req, res) => {
-    res.sendFile('./views/error.html', {root: __dirname});
+    res.render('error');
 });
